3f933865b2b3e07bb6ea6b3eb8c1fb266a941591
Jeffrey Wang <jqywang.95@gmail.com>
2018-03-22 10:52:48 -0700

    commented out other componenets for now

--- proxy/server.js
+++ proxy/server.js
@@ -12,70 +12,70 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/:listing_id', express.static(path.join(__dirname, 'public')));
 
-//bryan
-app.use('/reviews', (req, res) => {
-  axios.get(`http://reviews:3001${req.originalUrl}`)
-    .then(res => res.data)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
-});
+// //bryan
+// app.use('/reviews', (req, res) => {
+//   axios.get(`http://reviews:3001${req.originalUrl}`)
+//     .then(res => res.data)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send();
+//     });
+// });
 
-//eric
-app.use('/overview', (req, res) => {
-  axios.get(`http://overview:3002${req.originalUrl}`)
-    .then(res => res.data)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
-});
+// //eric
+// app.use('/overview', (req, res) => {
+//   axios.get(`http://overview:3002${req.originalUrl}`)
+//     .then(res => res.data)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send();
+//     });
+// });
 
-//kirk
-app.use('/nearby', (req, res) => {
-  axios.get(`http://nearby:3003${req.originalUrl}`)
-    .then(res => res.data)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
-});
+// //kirk
+// app.use('/nearby', (req, res) => {
+//   axios.get(`http://nearby:3003${req.originalUrl}`)
+//     .then(res => res.data)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send();
+//     });
+// });
 
-//kyle
-app.use('/q-and-a', (req, res) => {
-  axios.get(`http://q-and-a:3004${req.originalUrl}`)
-    .then(res => res.data)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
-});
+// //kyle
+// app.use('/q-and-a', (req, res) => {
+//   axios.get(`http://q-and-a:3004${req.originalUrl}`)
+//     .then(res => res.data)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send();
+//     });
+// });
 
-//zack
-app.use('/recommendations', (req, res) => {
-  axios.get(`http://recommendations:3005${req.originalUrl}`)
-    .then(res => res.data)
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.send();
-    });
-});
+// //zack
+// app.use('/recommendations', (req, res) => {
+//   axios.get(`http://recommendations:3005${req.originalUrl}`)
+//     .then(res => res.data)
+//     .then((data) => {
+//       res.send(data);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.send();
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`)
