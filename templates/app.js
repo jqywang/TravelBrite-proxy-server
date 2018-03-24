b26b9a5b6573934e33c2999241bc247cd5e10da4
Jeffrey Wang <jqywang.95@gmail.com>
2018-03-24 12:05:29 -0700

    started copying down the basic server side rendering files from example

---
+++ templates/app.js
@@ -0,0 +1,7 @@
+
+module.exports = (overview, nearby, reccomendations, reviews) => `
+  <div id="overview">${info}</div>
+  <div id="nearby">${countdown}</div>
+  <div id="reccomendations">${related}</div>
+  <div id="reviews">${reviews}</div>
+`;
